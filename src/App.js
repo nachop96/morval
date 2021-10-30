93ac660b0998890e0bf302d2ae02a4751f7e923d
Ignacio Pedrosa <ignapedrosa@gmail.com>
2021-10-30 20:26:26 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <img src='https://i.blogs.es/560885/game-over/450_1000.jpg'  alt="logo" />
-       {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Perdiste :(
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @SCOLY
-        </a>
-      </header>
+      <NavBar/>
+      
     </div>
   );
 }
